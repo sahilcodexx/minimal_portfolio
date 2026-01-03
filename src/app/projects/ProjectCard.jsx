43f9753e6bd404115fcb80348114fd20c68ab314
Sahil Singh <sahil171759@gmail.com>
2026-01-03 23:58:39 +0530

    motion effect added on cards

--- src/app/projects/ProjectCard.jsx
+++ src/app/projects/ProjectCard.jsx
@@ -17,6 +17,7 @@ import Github from "@/svgs/Github";
 import JavaScript from "@/technologies/JavaScript";
 import { ArrowRight } from "lucide-react";
 import { ProjectCardData } from "@/config/projects/ProjectCardData";
+import { motion as Motion } from "motion/react";
 const ProjectCard = () => {
   return (
     <Container
@@ -24,113 +25,125 @@ const ProjectCard = () => {
     >
       {ProjectCardData.map((items) => {
         return (
-          <Card
-            key={items.title}
-            className="group h-full w-full overflow-hidden border-gray-100 p-0 shadow-none transition-all dark:border-gray-800"
+          <Motion.div
+            initial={{ y: 40, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{
+              duration: 0.5,
+              ease: "easeInOut",
+              delay: 0.2 * items.id,
+            }}
+            viewport={{ once: true }}
+            className="flex flex-col gap-4"
           >
-            <CardHeader className="p-0">
-              <img
-                width={1920}
-                height={1080}
-                src={items.img.src}
-                alt={items.img.alt}
-              />
-            </CardHeader>
-            <CardContent>
-              <div className="space-y-4">
-                <div className="flex items-center justify-between gap-4">
-                  <Link href="">
-                    <h3 className="group-hover:text-primary text-xl leading-tight font-semibold hover:cursor-pointer">
-                      {items.title}
-                    </h3>
+            <Card
+              key={items.title}
+              className="group h-full w-full overflow-hidden border-gray-100 p-0 shadow-none transition-all dark:border-gray-800"
+            >
+              <CardHeader className="p-0">
+                <img
+                  width={1920}
+                  height={1080}
+                  src={items.img.src}
+                  alt={items.img.alt}
+                />
+              </CardHeader>
+              <CardContent>
+                <div className="space-y-4">
+                  <div className="flex items-center justify-between gap-4">
+                    <Link href="">
+                      <h3 className="group-hover:text-primary text-xl leading-tight font-semibold hover:cursor-pointer">
+                        {items.title}
+                      </h3>
+                    </Link>
+                    <div className="flex items-center gap-2">
+                      <Tooltip>
+                        <TooltipTrigger>
+                          <Link
+                            to={items.links.website}
+                            className="text-secondary hover:text-primary flex size-6 items-center justify-center transition-colors"
+                            href=""
+                            target="_blank"
+                          >
+                            <Website />
+                          </Link>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>View Website</p>
+                        </TooltipContent>
+                      </Tooltip>
+                      <Tooltip>
+                        <TooltipTrigger>
+                          <Link
+                            to={items.links.github}
+                            className="text-secondary hover:text-primary flex size-6 items-center justify-center transition-colors"
+                            href=""
+                            target="_blank"
+                          >
+                            <Github />
+                          </Link>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>View GitHub</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </div>
+                  </div>
+                  <p className="text-secondary line-clamp-3">
+                    {items.description}
+                  </p>
+                  <div>
+                    <h4 className="text-secondary mb-2 text-sm font-medium">
+                      Technologies
+                    </h4>
+                    <div className="flex flex-wrap gap-2">
+                      {items.technologies.map((tech) => {
+                        return (
+                          <Tooltip>
+                            <TooltipTrigger>
+                              <div className="size-6 transition-all duration-300 hover:scale-120 hover:cursor-pointer">
+                                {tech.icon}
+                              </div>
+                            </TooltipTrigger>
+                            <TooltipContent>{tech.name}</TooltipContent>
+                          </Tooltip>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </CardContent>
+              {items.details && (
+                <CardFooter className="flex items-center justify-between p-6 pt-0">
+                  <div
+                    className={`flex items-center gap-1 rounded-md px-2 py-1 text-xs ${
+                      items.isWorking
+                        ? "border-green-300 bg-green-500/10"
+                        : "border-red-300 bg-red-500/10"
+                    }`}
+                  >
+                    {items.isWorking ? (
+                      <>
+                        <div className="size-2 animate-pulse rounded-full bg-green-500" />
+                        All Systems Operational
+                      </>
+                    ) : (
+                      <>
+                        <div className="size-2 animate-pulse rounded-full bg-red-500" />
+                        Building
+                      </>
+                    )}
+                  </div>
+                  <Link
+                    to={items.projectDetailsPageSlug}
+                    className="text-secondary hover:text-primary flex items-center gap-2 text-sm underline-offset-4 transition-colors duration-200 ease-[ease] hover:underline"
+                  >
+                    View Details <ArrowRight className="size-4" />
                   </Link>
-                  <div className="flex items-center gap-2">
-                    <Tooltip>
-                      <TooltipTrigger>
-                        <Link
-                          to={items.links.website}
-                          className="text-secondary hover:text-primary flex size-6 items-center justify-center transition-colors"
-                          href=""
-                          target="_blank"
-                        >
-                          <Website />
-                        </Link>
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        <p>View Website</p>
-                      </TooltipContent>
-                    </Tooltip>
-                    <Tooltip>
-                      <TooltipTrigger>
-                        <Link
-                          to={items.links.github}
-                          className="text-secondary hover:text-primary flex size-6 items-center justify-center transition-colors"
-                          href=""
-                          target="_blank"
-                        >
-                          <Github />
-                        </Link>
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        <p>View GitHub</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </div>
-                </div>
-                <p className="text-secondary line-clamp-3">
-                  {items.description}
-                </p>
-                <div>
-                  <h4 className="text-secondary mb-2 text-sm font-medium">
-                    Technologies
-                  </h4>
-                  <div className="flex flex-wrap gap-2">
-                    {items.technologies.map((tech) => {
-                      return (
-                        <Tooltip>
-                          <TooltipTrigger>
-                            <div className="size-6 transition-all duration-300 hover:scale-120 hover:cursor-pointer">
-                              {tech.icon}
-                            </div>
-                          </TooltipTrigger>
-                          <TooltipContent>{tech.name}</TooltipContent>
-                        </Tooltip>
-                      );
-                    })}
-                  </div>
-                </div>
-              </div>
-            </CardContent>
-            {items.details && (
-              <CardFooter className="flex items-center justify-between p-6 pt-0">
-                <div
-                  className={`flex items-center gap-1 rounded-md px-2 py-1 text-xs ${
-                    items.isWorking
-                      ? "border-green-300 bg-green-500/10"
-                      : "border-red-300 bg-red-500/10"
-                  }`}
-                >
-                  {items.isWorking ? (
-                    <>
-                      <div className="size-2 animate-pulse rounded-full bg-green-500" />
-                      All Systems Operational
-                    </>
-                  ) : (
-                    <>
-                      <div className="size-2 animate-pulse rounded-full bg-red-500" />
-                      Building
-                    </>
-                  )}
-                </div>
-                <Link
-                  to={items.projectDetailsPageSlug}
-                  className="text-secondary hover:text-primary flex items-center gap-2 text-sm underline-offset-4 transition-colors duration-200 ease-[ease] hover:underline"
-                >
-                  View Details <ArrowRight className="size-4" />
-                </Link>
-              </CardFooter>
-            )}
-          </Card>
+                </CardFooter>
+              )}
+            </Card>
+          </Motion.div>
         );
       })}
     </Container>
