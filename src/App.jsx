56e50f7a20a2e7961f2c83c594e6dcd38cfbf981
Sahil Singh <sahil171759@gmail.com>
2026-01-23 17:49:35 +0530

    lenis added for smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,58 @@
-import { Route, Routes } from "react-router-dom";
-import { ThemeProvider } from "./components/landing/theme-provider";
-import Home from "./components/pages/Home";
-import { Navbar } from "./components/common/Navbar";
-import Blogs from "./components/pages/Blogs";
-import ProjectContent from "./app/projects/ProjectContent";
-import Work from "./components/pages/Work";
-import BlogContent from "./blog/BlogContent";
-import Projects from "./components/pages/Projects";
-import Footer from "./components/common/Footer";
-import { Quote } from "./components/common/Quote";
-import Contact from "./components/pages/Contact";
-import ResumePage from "./components/layouts/ResumePage";
-import Container from "./components/layouts/Container";
-import Layout from "./components/common/Layout";
-import GearsPage from "./app/gear/Gear";
+import { ReactLenis } from "lenis/react"
+import { Route, Routes } from "react-router-dom"
+import { ThemeProvider } from "./components/landing/theme-provider"
+import Home from "./components/pages/Home"
+import { Navbar } from "./components/common/Navbar"
+import Blogs from "./components/pages/Blogs"
+import ProjectContent from "./app/projects/ProjectContent"
+import Work from "./components/pages/Work"
+import BlogContent from "./blog/BlogContent"
+import Projects from "./components/pages/Projects"
+import Footer from "./components/common/Footer"
+import { Quote } from "./components/common/Quote"
+import Contact from "./components/pages/Contact"
+import ResumePage from "./components/layouts/ResumePage"
+import Container from "./components/layouts/Container"
+import Layout from "./components/common/Layout"
+import GearsPage from "./app/gear/Gear"
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Container>
-        <Layout>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/work" element={<Work />} />
-            <Route path="blogs" element={<Blogs />} />
-            <Route path="resume" element={<ResumePage />} />
-            <Route path="projects" element={<Projects />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="projects/:slug" element={<ProjectContent />} />
-            <Route path="blogs/:slug" element={<BlogContent />} />
-            <Route path="gear" element={<GearsPage />} />
-          </Routes>
-          <Quote />
-          <Footer />
-        </Layout>
-      </Container>
+      <ReactLenis
+        root={true}
+        options={{
+          lerp: 0.1,
+          duration: 1.2,
+          orientation: "vertical",
+          gestureOrientation: "vertical",
+          smoothWheel: true,
+          wheelMultiplier: 1,
+          smoothTouch: false,
+          touchMultiplier: 2,
+        }}
+      >
+        <Container>
+          <Layout>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/work" element={<Work />} />
+              <Route path="blogs" element={<Blogs />} />
+              <Route path="resume" element={<ResumePage />} />
+              <Route path="projects" element={<Projects />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="projects/:slug" element={<ProjectContent />} />
+              <Route path="blogs/:slug" element={<BlogContent />} />
+              <Route path="gear" element={<GearsPage />} />
+            </Routes>
+            <Quote />
+            <Footer />
+          </Layout>
+        </Container>
+      </ReactLenis>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
