7039d472e16e623f41140f530a5629654c191ccd
Sahil Singh <sahil171759@gmail.com>
2026-01-29 07:59:46 +0530

    posthog fix

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById("root")).render(
         api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
         capture_pageview:false,
         capture_exceptions: true,
-        debug: import.meta.env.MODE === "production",
+        debug: import.meta.env.MODE === "development",
       }}
     >
       <BrowserRouter>
